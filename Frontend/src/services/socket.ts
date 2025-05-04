04246068b39f25476207d70fe51feb4f65915d98
Mighty-TR <talharehan10@gmail.com>
2025-05-04 17:00:26 +0500

    Fixed frontend packages

--- Frontend/src/services/socket.ts
+++ Frontend/src/services/socket.ts
@@ -1,6 +1,9 @@
-
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_API_URL); 
+const socket = io(import.meta.env.VITE_API_URL, {
+  path: "/socket.io",
+  transports: ["websocket"], // ensure direct WebSocket connection
+  withCredentials: true
+});
 
 export default socket;
